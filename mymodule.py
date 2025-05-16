324e432a0afc31f42bcebcdd55c442d6dc6a8a63
Endermanik13 <119730649+Endermanik13@users.noreply.github.com>
2025-05-16 10:22:18 +0300

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,11 @@
+def calculate_sum(a, b):
+    """Складывает два числа.
+
+    Args:
+        a (int): Первое число.
+        b (int): Второе число.
+
+    Returns:
+        int: Сумма a и b.
+    """
+    return a + b
